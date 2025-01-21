63e3cf85ace0bdd557698e570a75c127bd789c41
Beniamino Green <ben@greendalba.com>
2025-01-21 15:09:12 -0500

    refactor to allow for extension libraries

--- R/euclidean_join_core.R
+++ R/euclidean_join_core.R
@@ -11,7 +11,8 @@ multi_by_validate <- function(a, b, by) {
 
   if (is.null(by)) {
     by_a <- intersect(names(a), names(b))
-    by_b <- intersect(names(a), names(b))
+    by_b <- by_a
+    stopifnot("Can't Determine columns to match on" = length(by_a)!=0)
   } else {
     if (!is.null(names(by))) {
       by_a <- names(by)
@@ -29,13 +30,10 @@ multi_by_validate <- function(a, b, by) {
   ))
 }
 
-# ` @importFrom stats pnorm
-euclidean_join_core <- function(a, b, by = NULL, n_bands = 30, band_width = 10, threshold = 1.0, r = .5, progress = FALSE, mode = "inner") {
-  stopifnot("'radius' must be greater than 0" = threshold > 0)
 
-  by <- multi_by_validate(a, b, by)
-  by_a <- by[[1]]
-  by_b <- by[[2]]
+# ` @importFrom stats pnorm
+euclidean_join <- function(a, b, by_a, by_b, block_by_a = NULL, block_by_b = NULL, n_bands = 30, band_width = 10, threshold = 1.0, r = .5, progress = FALSE) {
+  stopifnot("'radius' must be greater than 0" = threshold > 0)
   stopifnot("There should be no NA's in by_a[1]" = !anyNA(a[[by_a[1]]]))
   stopifnot("There should be no NA's in by_a[2]" = !anyNA(a[[by_a[2]]]))
   stopifnot("There should be no NA's in by_b[1]" = !anyNA(b[[by_b[1]]]))
@@ -63,33 +61,5 @@ euclidean_join_core <- function(a, b, by = NULL, n_bands = 30, band_width = 10, 
     seed = round(runif(1, 0, 2^32))
   )
 
-  names_in_both <- intersect(names(a), names(b))
-
-  names(a)[names(a) %in% names_in_both] <-
-    paste0(names(a)[names(a) %in% names_in_both], ".x")
-  names(b)[names(b) %in% names_in_both] <-
-    paste0(names(b)[names(b) %in% names_in_both], ".y")
-
-  matches <- dplyr::bind_cols(a[match_table[, 1], ], b[match_table[, 2], ])
-
-  # No need to look for rows that don't match
-  if (mode == "inner") {
-    return(matches)
-  }
-
-  not_matched_a <- !seq(nrow(a)) %in% match_table[, 1]
-  not_matched_b <- !seq(nrow(b)) %in% match_table[, 2]
-
-  if (mode == "left") {
-    matches <- dplyr::bind_rows(matches, a[not_matched_a, ])
-  } else if (mode == "right") {
-    matches <- dplyr::bind_rows(matches, b[not_matched_b, ])
-  } else if (mode == "full") {
-    matches <- dplyr::bind_rows(matches, a[not_matched_a, ], b[not_matched_b, ])
-  } else if (mode == "anti") {
-    matches <- dplyr::bind_rows(a[not_matched_a, ], b[not_matched_b, ])
-  } else {
-    stop("Invalid Mode Selected!")
-  }
-  return(matches)
+  return(list(match_table = match_table))
 }
--- R/join_core.R
+++ R/join_core.R
@@ -1,6 +1,82 @@
+#' Core Fuzzy Joining Logic
+#'
+#' @param a the first dataframe to join
+#' @param b the second dataframe to join
+#' @param by columns on which to join
+#' @param join_func the joining function responsible for performing the join
+#' @param mode the dplyr-style type of join you want to perform
+#' @param block_on any columns to block (perform exact matching on) (where supported)
+#' @param ... Other parameters to be passed to the joining function
+#'
+#' @importFrom dplyr pull %>%
+fuzzy_join_core <- function(a, b, by, join_func, mode, block_by = NULL, similarity_column = NULL, ...) {
+  a <- tibble::as_tibble(a)
+  b <- tibble::as_tibble(b)
+
+  by <- multi_by_validate(a, b, by)
+  by_a <- by[[1]]
+  by_b <- by[[2]]
+
+  if (!is.null(block_by)) {
+      block_by <- multi_by_validate(a, b, block_by)
+      block_by_a <- block_by[[1]]
+      block_by_b <- block_by[[2]]
+  } else {
+      block_by_a <- NULL
+      block_by_b <- NULL
+  }
+
+  match_result <- join_func(
+                           a=a, b=b,
+                           by_a = by_a,by_b = by_b,
+                           block_by_a = block_by_a ,block_by_b = block_by_b,
+                           ...)
+
+  match_table <- match_result[['match_table']]
+  similarities <- match_result[['similarities']]
+
+  # Rename Columns in Both Tables
+  names_in_both <- intersect(names(a), names(b))
+  names(a)[names(a) %in% names_in_both] <- paste0(names(a)[names(a) %in% names_in_both], ".x")
+  names(b)[names(b) %in% names_in_both] <- paste0(names(b)[names(b) %in% names_in_both], ".y")
+
+  matches <- dplyr::bind_cols(a[match_table[, 1], ], b[match_table[, 2], ])
+
+  if (!is.null(similarity_column)) {
+    matches[, similarity_column] <- similarities
+  }
+
+  # No need to look for rows that don't match
+  if (mode == "inner") {
+    return(matches)
+  }
+
+  switch(mode,
+    "left" = {
+      not_matched_a <- collapse::`%!iin%`(seq_len(nrow(a)), match_table[, 1])
+      matches <- dplyr::bind_rows(matches, a[not_matched_a, ])
+    },
+    "right" = {
+      not_matched_b <- collapse::`%!iin%`(seq_len(nrow(b)), match_table[, 2])
+      matches <- dplyr::bind_rows(matches, b[not_matched_b, ])
+    },
+    "full" = {
+      not_matched_a <- collapse::`%!iin%`(seq_len(nrow(a)), match_table[, 1])
+      not_matched_b <- collapse::`%!iin%`(seq_len(nrow(b)), match_table[, 2])
+      matches <- dplyr::bind_rows(matches, a[not_matched_a, ], b[not_matched_b, ])
+    },
+    "anti" = {
+      not_matched_a <- collapse::`%!iin%`(seq_len(nrow(a)), match_table[, 1])
+      not_matched_b <- collapse::`%!iin%`(seq_len(nrow(b)), match_table[, 2])
+      matches <- dplyr::bind_rows(a[not_matched_a, ], b[not_matched_b, ])
+    }
+  )
+
+  matches
+}
 
 #' @importFrom dplyr pull %>%
-jaccard_join  <- function(a, b, by_a, by_b, block_by_a, block_by_b, n_gram_width, n_bands,
+jaccard_join <- function(a, b, by_a, by_b, block_by_a, block_by_b, n_gram_width, n_bands,
                           band_width, threshold, progress = FALSE, a_salt = NULL, b_salt = NULL,
                          clean = FALSE) {
 
